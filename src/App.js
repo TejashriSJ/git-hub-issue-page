a58ce7d016f845acd330fa14219d174047371113
TejashriSJ <tejashri.j.23.2@mountblue.tech>
2023-04-13 15:42:32 +0530

    Update App js file
    This will modify the code to check loading and error page

--- src/App.js
+++ src/App.js
@@ -6,12 +6,35 @@ import Header from "./Components/Header";
 import "./App.css";
 
 class App extends Component {
+  state = { Issues: [], isLoading: true, isError: false };
+  componentDidMount() {
+    fetch("https://api.github.com/repos/rails/rails/issues")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({ Issues: data, isLoading: false });
+      })
+      .catch((err) => {
+        this.setState({ isLoading: false, isError: true });
+        console.log(err);
+      });
+  }
   render() {
     return (
       <Router>
         <Header />
         <Routes>
-          <Route path="/" Component={Issues} />
+          <Route
+            path="/"
+            element={
+              <Issues
+                issues={this.state.Issues}
+                isLoading={this.state.isLoading}
+                isError={this.state.isError}
+              />
+            }
+          />
         </Routes>
       </Router>
     );
