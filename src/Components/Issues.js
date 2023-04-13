25ddc51418160742458ff481a9e71119b2ef45f8
TejashriSJ <tejashri.j.23.2@mountblue.tech>
2023-04-13 23:23:02 +0530

    Update Issues file
    This will modify the code to display all page details

--- src/Components/Issues.js
+++ src/Components/Issues.js
@@ -2,13 +2,45 @@ import React, { Component } from "react";
 import Issue from "./Issue";
 
 class Issues extends Component {
+  state = { Issues: [], isLoading: true, isError: false };
+  componentDidMount() {
+    fetch(
+      `https://api.github.com/repos/rails/rails/issues?page=${this.props.pageNumber}`
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({ Issues: data, isLoading: false });
+      })
+      .catch((err) => {
+        this.setState({ isLoading: false, isError: true });
+        console.log(err);
+      });
+  }
+  componentDidUpdate() {
+    fetch(
+      `https://api.github.com/repos/rails/rails/issues?page=${this.props.pageNumber}`
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({ Issues: data, isLoading: false });
+      })
+      .catch((err) => {
+        this.setState({ isLoading: false, isError: true });
+        console.log(err);
+      });
+  }
+
   render() {
-    if (this.props.isLoading) {
+    if (this.state.isLoading) {
       return <h1>Loading ...</h1>;
-    } else if (this.props.isError) {
+    } else if (this.state.isError) {
       return <h1>Error in fetching the data</h1>;
     } else {
-      return this.props.issues.map((issue) => {
+      return this.state.Issues.map((issue) => {
         return <Issue key={issue.id} issue={issue} />;
       });
     }
