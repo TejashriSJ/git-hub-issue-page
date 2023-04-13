3cb10cb81d70df96112d4a88e4c2b0395e1df777
TejashriSJ <tejashri.j.23.2@mountblue.tech>
2023-04-13 15:44:52 +0530

    Update Header file
    This will modify the header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,16 +4,19 @@ import React, { Component } from "react";
 class Header extends Component {
   render() {
     return (
-      <nav>
-        <ul className="issue-header">
-          <li>Author</li>
-          <li>Label</li>
-          <li>Projects</li>
-          <li>Milestones</li>
-          <li>Assigne</li>
-          <li>sort</li>
-        </ul>
-      </nav>
+      <div>
+        <h1 className="header-heading">Git Hub Errors Page</h1>
+        <nav>
+          <ul className="issue-header">
+            <li>Author</li>
+            <li>Label</li>
+            <li>Projects</li>
+            <li>Milestones</li>
+            <li>Assigne</li>
+            <li>sort</li>
+          </ul>
+        </nav>
+      </div>
     );
   }
 }
